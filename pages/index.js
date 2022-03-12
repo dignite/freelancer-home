9bb7fb9810163f376327bf2beff255091bd7b73c
Daniel Edholm Ignat <daniel@dignat.se>
2022-03-12 16:45:15 +0100

    Add harvest-report-lambda call

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,29 @@
 import Link from "next/link";
 
-export default function IndexPage() {
+export default function IndexPage({ data }) {
   return (
     <div>
-      Freelancer Home.{" "}
+      <h1>Freelancer Home</h1>
       <Link href="/about">
         <a>About</a>
       </Link>
+      <h2>Unbilled invoice</h2>
+      <p>{data.meta.unbilledInvoice.excludingVAT} excluding VAT</p>
+      <h2>Total unbilled hours per week</h2>
+      <ul>
+        {Object.keys(data.meta.totalUnbilledHoursPerWeek).map((week) => (
+          <li key={week}>
+            {week}: {data.meta.totalUnbilledHoursPerWeek[week]}
+          </li>
+        ))}
+        <li>Total: {data.meta.totalUnbilledHours}</li>
+      </ul>
     </div>
   );
+}
+
+export async function getServerSideProps() {
+  const res = await fetch(process.env.HARVEST_REPORT_LAMBDA_HOURS_URL);
+  const data = await res.json();
+  return { props: { data } };
 }
