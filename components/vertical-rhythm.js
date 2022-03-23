384601617d856e39d9deced1afc2620fe817f985
Daniel Edholm Ignat <daniel@dignat.se>
2022-03-23 18:54:40 +0100

    Introduce UnorderedList

--- components/vertical-rhythm.js
+++ components/vertical-rhythm.js
@@ -22,3 +22,19 @@ export const Paragraph = ({ children }) => (
     {children}
   </p>
 );
+
+export const UnorderedList = ({ children }) => (
+  <ul>
+    <style jsx>{`
+      ul {
+        margin-top: 0;
+        line-height: var(--rhythm2);
+        padding-top: var(--typography__pOffset);
+        margin-bottom: calc(var(--rhythm2) - var(--typography__pOffset));
+      }
+    `}</style>
+    {children}
+  </ul>
+);
+
+export const UnorderedListItem = ({ children }) => <li>{children}</li>;
