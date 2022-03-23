cedf8f1a3e2ebb40cc2af3a35ba650e8ef6436cd
Daniel Edholm Ignat <daniel@dignat.se>
2022-03-23 18:48:34 +0100

    Introduce ApplicationContainer

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,11 @@
 import "normalize.css";
+import { ApplicationContainer } from "../components/vertical-rhythm";
 import "../vertical-rhythm.css";
 
 export default function FreelancerHome({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ApplicationContainer>
+      <Component {...pageProps} />
+    </ApplicationContainer>
+  );
 }
