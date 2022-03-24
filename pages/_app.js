eda27aa560fff29c537957ce261ac0e05d197e60
Daniel Edholm Ignat <daniel@dignat.se>
2022-03-24 18:56:27 +0100

    Move navigation to _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,31 @@
 import "normalize.css";
-import { ApplicationContainer } from "../components/vertical-rhythm";
 import "../vertical-rhythm.css";
+
+import {
+  ApplicationContainer,
+  Heading,
+  UnorderedList,
+  UnorderedListItem,
+} from "../components/vertical-rhythm";
+import Link from "next/link";
 
 export default function FreelancerHome({ Component, pageProps }) {
   return (
     <ApplicationContainer>
       <Component {...pageProps} />
+      <Heading>Navigation</Heading>
+      <UnorderedList>
+        <UnorderedListItem>
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+        </UnorderedListItem>
+        <UnorderedListItem>
+          <Link href="/about">
+            <a>About</a>
+          </Link>
+        </UnorderedListItem>
+      </UnorderedList>
     </ApplicationContainer>
   );
 }
