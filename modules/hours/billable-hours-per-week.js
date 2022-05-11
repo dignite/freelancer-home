dbe842c441ca047afa155d6602c7641f9f04b82a
Daniel Edholm Ignat <daniel@dignat.se>
2022-05-11 18:49:27 +0200

    Adjust api calls and variable/component/copy to match

--- modules/hours/billable-hours-per-week.js
+++ modules/hours/billable-hours-per-week.js
@@ -6,23 +6,23 @@ import {
   TableRow,
 } from "../layout/vertical-rhythm";
 
-export const UnbilledHoursPerWeek = ({ meta }) => (
+export const BillableHoursPerWeek = ({ meta }) => (
   <>
-    <Heading>Total unbilled hours per week</Heading>
+    <Heading>Total billable hours per week</Heading>
     <Table>
       <TableRow>
         <TableHeader>Week</TableHeader>
         <TableHeader>Hours</TableHeader>
       </TableRow>
-      {Object.keys(meta.totalUnbilledHoursPerWeek).map((week) => (
+      {Object.keys(meta.totalBillableHoursPerWeek).map((week) => (
         <TableRow key={week}>
           <TableData>{week}</TableData>
-          <TableData>{meta.totalUnbilledHoursPerWeek[week]}</TableData>
+          <TableData>{meta.totalBillableHoursPerWeek[week]}</TableData>
         </TableRow>
       ))}
       <TableRow>
         <TableData>Total</TableData>
-        <TableData>{meta.totalUnbilledHours}</TableData>
+        <TableData>{meta.totalBillableHours}</TableData>
       </TableRow>
     </Table>
   </>
