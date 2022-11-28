510c0f4301d199e311f4df7fbec2aa35808b8956
Daniel Edholm Ignat <daniel.edholm.ignat@tradera.com>
2022-11-28 19:39:50 +0100

    Forward startDate and endDate to getInvoice
    Otherwise ajax refresh won't work

--- pages/api/invoice/[startDate]/[endDate].js
+++ pages/api/invoice/[startDate]/[endDate].js
@@ -1,5 +1,6 @@
 export default async function handler(req, res) {
-  const invoice = await getInvoice();
+  const { startDate, endDate } = req.query;
+  const invoice = await getInvoice(startDate, endDate);
   res.status(200).json(invoice);
 }
 
