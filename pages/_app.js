b07a81b5cb136721526389b9ed86d981457288bd
Daniel Edholm Ignat <daniel.edholm.ignat@tradera.com>
2023-03-25 19:26:39 +0100

    Install react-query and use for month page

--- pages/_app.js
+++ pages/_app.js
@@ -9,29 +9,31 @@ import {
   UnorderedListItem,
 } from "../modules/layout/vertical-rhythm";
 import Link from "next/link";
+import { useState } from "react";
+import { Hydrate, QueryClient, QueryClientProvider } from "react-query";
 
 export default function FreelancerHome({ Component, pageProps }) {
+  const [queryClient] = useState(() => new QueryClient());
+
   return (
-    <ApplicationContainer>
-      <Component {...pageProps} />
-      <Heading>Navigation</Heading>
-      <UnorderedList>
-        <UnorderedListItem>
-          <Link href="/">
-            Home
-          </Link>
-        </UnorderedListItem>
-        <UnorderedListItem>
-          <Link href="/day">
-            Day
-          </Link>
-        </UnorderedListItem>
-        <UnorderedListItem>
-          <Link href="/month">
-            Month
-          </Link>
-        </UnorderedListItem>
-      </UnorderedList>
-    </ApplicationContainer>
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <ApplicationContainer>
+          <Component {...pageProps} />
+          <Heading>Navigation</Heading>
+          <UnorderedList>
+            <UnorderedListItem>
+              <Link href="/">Home</Link>
+            </UnorderedListItem>
+            <UnorderedListItem>
+              <Link href="/day">Day</Link>
+            </UnorderedListItem>
+            <UnorderedListItem>
+              <Link href="/month">Month</Link>
+            </UnorderedListItem>
+          </UnorderedList>
+        </ApplicationContainer>
+      </Hydrate>
+    </QueryClientProvider>
   );
 }
