669a1eb6f0248485b7065d442a5300a3d7c920fe
Daniel Edholm Ignat <daniel.edholm.ignat@tradera.com>
2023-03-27 20:04:24 +0200

    Make by-name more dynamic

--- pages/api/by-name/[name]/[startDate]/[endDate].js
+++ pages/api/by-name/[name]/[startDate]/[endDate].js
@@ -1,7 +1,7 @@
 import { byName } from "../../../../../modules/harvest-report-api/index";
 
 export default async function handler(req, res) {
-  const { startDate, endDate } = req.query;
-  const hours = await byName(startDate, endDate, "VAB");
+  const { name, startDate, endDate } = req.query;
+  const hours = await byName(startDate, endDate, name);
   res.status(200).json(hours);
 }
